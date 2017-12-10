f332dc60ecd7b4f547528ca77eceddc28c2ea8f4
Tuanhao <tuanhao256@gmail.com>
2017-12-11 06:54:24 +0800

    added security

--- src/App.js
+++ src/App.js
@@ -3,19 +3,48 @@ import './App.css';
 import GameBoard from './GameBoard.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      passedSecurityCheck: false,
+      password: '',
+      abc: ''
+    }
+  }
+
+  securityCheck() {
+    if (this.state.password === '967456852159hao') {
+      this.setState({ passedSecurityCheck: true })
+    }
+  }
+
+  typingPassword(e) {
+    this.setState({
+      password: e.target.value,
+    })
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-logo">PD</h1>
-          <h1 className="App-title">PHÓN DẬU</h1>
-        </header>
-        <p className="App-intro">
-          Hãy trả lời câu hỏi thật nhanh để được phần thưởng hấp dẫn!
-        </p>
-        <GameBoard />
-      </div>
-    );
+    if(this.state.passedSecurityCheck) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-logo">PD</h1>
+            <h1 className="App-title">PHÓN DẬU</h1>
+          </header>
+          <p className="App-intro">
+            Hãy trả lời câu hỏi thật nhanh để được phần thưởng hấp dẫn!
+          </p>
+          <GameBoard />
+        </div>
+      );
+    } else {
+      return(
+        <div>
+          <input type="password" onChange={this.typingPassword.bind(this)} value={this.state.password}></input>
+          <button onClick={this.securityCheck.bind(this)}>Submit</button>
+        </div>
+      )
+    }
   }
 }
 
